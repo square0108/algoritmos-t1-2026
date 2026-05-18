267cc3f985b376382ad610f7ed6b642aedbd7609
square0108 <martinmariollanos@gmail.com>
2026-05-18 04:17:09 -0400

    Ejecucion cli de multiplicaciones para testing

--- src/test.cpp
+++ src/test.cpp
@@ -1,24 +1,38 @@
 #include "matrix.h"
+#include "strassen.h"
+#include <string>
+#include <cstdlib>
 #include <iostream>
 
-int main() {
-	Matrix<int> *A = new Matrix<int>(8,8);
-	Matrix<int> *B = new Matrix<int>(8,8);
-	for (int i = 0; i < A->rows; i++) {
-		for (int j = 0; j < A->cols; j++) {
-			(*A)[i][j] = i+j+1;
-			(*B)[i][j] = i+j+1;
-		}
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		std::cerr << "Usage: ./executable [strassen | rowcol | hybrid] [n_matrix_size]" << std::endl;
+		exit(-1);
 	}
-	std::cout << (*A)[0][0] << std::endl;
-	std::cout << "Matrix rows: " << A->rows << "; Matrix cols: " << A->cols << std::endl;
-	A->print_contents();
+	const std::string mult_type = argv[1];
+	const unsigned int MATRIX_SIZE = atoi(argv[2]);
+	/*
 	std::cout << "Partition test" << std::endl;
 	ShallowPartition<int> part(*A,4,4,std::make_pair(4,4));
 	part.print_contents();
 	std::cout << "copy_block_matrix test" << std::endl;
-	matrix_block_copy(*A,part,std::make_pair(7,7));
+	matrix_block_copy(*A,part,std::make_pair(3,3));
 	A->print_contents();
-
+	*/
+	std::cout << "matrix mult test (no print)..." << std::endl;
+	Matrix<int> *D = new Matrix<int>(MATRIX_SIZE,MATRIX_SIZE);
+	Matrix<int> *E = new Matrix<int>(MATRIX_SIZE,MATRIX_SIZE);
+	Matrix<int> *F = new Matrix<int>(MATRIX_SIZE,MATRIX_SIZE);
+	for (int i = 0; i < D->rows; i++) {
+		for (int j = 0; j < D->cols; j++) {
+			(*D)(i,j) = i+j+1;
+			(*E)(i,j) = i+j+1;
+		}
+	}
+	if (mult_type == "strassen") strassen_mult(*F,*D,*E);
+	else if (mult_type == "rowcol") matrix_multRowCol(*F,*D,*E);
+	std::cout << "...success!" << std::endl;
+	F->print_contents();
+	delete D; delete E; delete F;
 	return 0;
 }
