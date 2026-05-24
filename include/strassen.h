52f761a0d2fdb97c4eaa0ab932f34b958ebbd639
square0108 <martinmariollanos@gmail.com>
2026-05-24 19:15:52 -0400

    Optimización menor en timing de uso de - Solamente hacer  a medida que se necesiten las matrices M1 ... M7 en vez de reservar las 7 de una. Comparando resultados de las dos branches, esto ahorró como 1 segundo en n0 = 1.

--- include/strassen.h
+++ include/strassen.h
@@ -35,9 +35,6 @@ void strassen_mult(MatC& output, MatA& A, MatB& B, size_t n0_threshold) {
 		PartitionView<DataType> C11(output,n/2,n/2,get_quad_coords(q11,n)); PartitionView<DataType> C12(output,n/2,n/2,get_quad_coords(q12,n));
 		PartitionView<DataType> C21(output,n/2,n/2,get_quad_coords(q21,n)); PartitionView<DataType> C22(output,n/2,n/2,get_quad_coords(q22,n));
 
-		// Arreglo para matrices M_1 ... M_7
-		Matrix<DataType>* Mmat[7];
-		for (int i = 0; i < 7; i++) Mmat[i] = new Matrix<DataType>(n/2,n/2);
 		// Matrices temporales para computar sumas
 		Matrix<DataType>* temp1 = new Matrix<DataType>(n/2,n/2);
 		Matrix<DataType>* temp2 = new Matrix<DataType>(n/2,n/2);
@@ -45,47 +42,51 @@ void strassen_mult(MatC& output, MatA& A, MatB& B, size_t n0_threshold) {
 		// M_1 = (A11+A22)*(B11+B22)
 		matrix_sum(*temp1,A11,A22);
 		matrix_sum(*temp2,B11,B22);
-		strassen_mult<DataType>(*Mmat[0],*temp1,*temp2,n0_threshold);
-
+		Matrix<DataType>* M1 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M1,*temp1,*temp2,n0_threshold);
 		// M_2
 		matrix_sum(*temp1,A21,A22);
-		strassen_mult<DataType>(*Mmat[1],*temp1,B11,n0_threshold);	
+		Matrix<DataType>* M2 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M2,*temp1,B11,n0_threshold);	
 		// M_3
 		matrix_sub(*temp1,B12,B22);
-		strassen_mult<DataType>(*Mmat[2],A11,*temp1,n0_threshold);
+		Matrix<DataType>* M3 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M3,A11,*temp1,n0_threshold);
 		// M_4
 		matrix_sub(*temp1,B21,B11);
-		strassen_mult<DataType>(*Mmat[3],A22,*temp1,n0_threshold);
+		Matrix<DataType>* M4 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M4,A22,*temp1,n0_threshold);
 		// M_5
 		matrix_sum(*temp1,A11,A12);
-		strassen_mult<DataType>(*Mmat[4],*temp1,B22,n0_threshold);
+		Matrix<DataType>* M5 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M5,*temp1,B22,n0_threshold);
 		// M_6
 		matrix_sub(*temp1,A21,A11);
 		matrix_sum(*temp2,B11,B12);
-		strassen_mult<DataType>(*Mmat[5],*temp1,*temp2,n0_threshold);
+		Matrix<DataType>* M6 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M6,*temp1,*temp2,n0_threshold);
 		// M_7
 		matrix_sub(*temp1,A12,A22);
 		matrix_sum(*temp2,B21,B22);
-		strassen_mult<DataType>(*Mmat[6],*temp1,*temp2,n0_threshold);
+		Matrix<DataType>* M7 = new Matrix<DataType>(n/2,n/2);
+		strassen_mult<DataType>(*M7,*temp1,*temp2,n0_threshold);
 
+		delete temp1; delete temp2;
 		// Cálculo de los elementos de C. Como las particiones son vistas a la data de la matriz original, escribir en C11 escribe directamente a C.
 		// C_1,1
-		matrix_sum(C11, *Mmat[0], *Mmat[3]);
-		matrix_sub(C11, C11, *Mmat[4]);
-		matrix_sum(C11, C11, *Mmat[6]);
+		matrix_sum(C11, *M1, *M4);
+		matrix_sub(C11, C11, *M5);
+		matrix_sum(C11, C11, *M7);
 		// C_1,2
-		matrix_sum(C12, *Mmat[2], *Mmat[4]);
+		matrix_sum(C12, *M3, *M5);
 		// C_2,1
-		matrix_sum(C21, *Mmat[1], *Mmat[3]);
+		matrix_sum(C21, *M2, *M4);
 		// C_2,2
-		matrix_sub(C22, *Mmat[0], *Mmat[1]);
-		matrix_sum(C22, C22, *Mmat[2]);
-		matrix_sum(C22, C22, *Mmat[5]);
+		matrix_sub(C22, *M1, *M2);
+		matrix_sum(C22, C22, *M3);
+		matrix_sum(C22, C22, *M6);
 
-		// Cleanup de memoria
-		for (int i = 0; i < 7; i++) delete Mmat[i];
-		delete temp1;
-		delete temp2;
+		delete M1; delete M2; delete M3; delete M4; delete M5; delete M6; delete M7;
 	}
 	// aqui se "retorna" output
 }
